3e60184eb2b33807436d97be8b5c205f3b28f33a
Jesusmk_ <61255024+Jesusm1025@users.noreply.github.com>
2024-10-31 14:53:06 -0400

    Add files via upload

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiUsuario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiUsuario.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; } 
+    }
+}
